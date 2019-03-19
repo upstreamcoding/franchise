56e50a777e365bb8d886e90984d8eecbcbb8b2ec
Alan Mabry <alan.mabry@gmail.com>
2019-03-19 17:28:12 -0400

    zipcode modal

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
-  $('#go').click(function(){
-    var zipcode = $('#zipcode').val();
+function getZipcode(zipcode) {
+  console.log(zipcode);
 
-    alert(zipcode);
-  });
+  $('#modal--zipcode').val(zipcode);
+}
+
+$(document).ready(function() {
+   // Event Handlers
+   $('#go-btn').click(function(){
+     var zipcode = $('#zipcode').val();
+
+     getZipcode(zipcode);
+   });
 });
